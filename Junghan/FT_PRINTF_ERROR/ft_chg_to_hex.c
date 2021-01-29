b0eefb5fe4550d8c0c47e7236a6a49b11ae35e5c
junghan <42junghan@gmail.com>
2021-01-29 20:10:50 +0900

    printferror

---
+++ Junghan/FT_PRINTF_ERROR/ft_chg_to_hex.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_chg_to_hex.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junghan <junghan@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/28 23:33:27 by junghan           #+#    #+#             */
+/*   Updated: 2021/01/29 18:20:47 by junghan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_chg_to_hex(unsigned int num, const char *spec)
+{
+	char	*hex;
+	char	*result;
+	int		idx;
+
+	if (!(result = (char *)ft_calloc(sizeof(char), 8)))
+		return (0);
+	if (*spec == 'x' || *spec == 'p')
+		hex = "0123456789abcdef";
+	else if (*spec == 'X')
+		hex = "0123456789ABCDEF";
+	idx = 7;
+	while (idx >= 0)
+	{
+		result[idx] = hex[num % 16];
+		num = num / 16;
+		idx--;
+	}
+	return (result);
+}
---
+++ Junghan/FT_PRINTF_ERROR/ft_parse_arg.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_arg.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junghan <junghan@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 12:33:56 by junghan           #+#    #+#             */
+/*   Updated: 2021/01/29 18:48:03 by junghan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_parse_arg(va_list ap, const char *format)
+{
+	const char	*curr;
+	t_info		spec_info;
+
+	curr = format;
+	while (*curr)
+	{
+		if (*curr == '%')
+		{
+			curr++;
+			ft_init_info(&spec_info);
+			if (!(curr = (const char *)ft_check_info(ap, curr, &spec_info)))
+				return (-1);
+			if (!(curr = ft_manage_spec(ap, curr, spec_info)))
+				return (-1);
+		}
+		else
+			ft_putchar_fd(*curr, 1);
+		curr++;
+	}
+	return (0);
+}
