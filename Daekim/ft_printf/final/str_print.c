f05dba5efd6284d137bae47ae5029062cc511f7d
Daehyun Kim <daekim@c2r3s7.42seoul.kr>
2021-02-02 23:20:20 +0900

    finish

--- Daekim/ft_printf/final/str_print.c
+++ Daekim/ft_printf/final/str_print.c
@@ -6,23 +6,27 @@
 /*   By: daekim <daekim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 21:39:32 by daekim            #+#    #+#             */
-/*   Updated: 2021/02/02 14:28:38 by daekim           ###   ########.fr       */
+/*   Updated: 2021/02/02 13:04:46 by daekim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			print_num_morz(t_cond *con, int len, int *ret)
+int			print_morz(t_cond *con, int len, int *ret)
 {
-	int		f_len;
+	int		l;
 
-	if (con->width > con->pre && con->minus_pre == 0)
-		f_len = con->width - len;
-	else
-		f_len = con->pre - len;
-	while (f_len-- > 0)
+	if (con->minus_pre == 0)
 	{
-		if (con->zero == 1 && con->dot == 0)
+		l = con->width - len;
+		if (con->dot == 1 && con->pre == 0)
+			l = con->width;
+	}
+	else
+		l = con->width;
+	while (l-- > 0)
+	{
+		if (con->zero == 1 && con->minus == 0)
 			write(1, "0", 1);
 		else
 			write(1, " ", 1);
@@ -31,36 +35,42 @@ int			print_num_morz(t_cond *con, int len, int *ret)
 	return (*ret);
 }
 
-int			n_prin(char *str, t_cond *con, int len, int *ret)
+int			s_prin(char *str, t_cond *con, int len, int *ret)
 {
 	int		i;
 
 	i = 0;
 	while (i < len)
 	{
-		if (con->dot == 1 && con->pre == 0 && *str == '0')
-		{
-			write(1, " ", 1);
-			(*ret)++;
+		if (con->dot == 1 && con->pre == 0)
 			break ;
-		}
 		write(1, &str[i++], 1);
 		(*ret)++;
 	}
 	return (*ret);
 }
 
-int			print_num(char *str, t_cond *con, int len, int *ret)
+int			print_str(char *str, t_cond *con, int len, int *ret)
 {
 	int		i;
 
 	i = 0;
-	if (con->dot == 1 && con->width == 0 && con->pre == 0 && *str == '0')
-		return (*ret);
-	if (con->minus == 1 || con->minus_pre == 1)
-		*ret = n_prin(str, con, len, ret);
-	*ret = print_num_morz(con, len, ret);
+	if (con->minus_pre == 1)
+	{
+		if (con->p_star == 1 && con->width < con->pre)
+		{
+			con->dot = 0;
+			con->minus_pre = 0;
+			con->pre = con->width;
+		}
+		else
+			con->width = con->pre;
+		con->pre = 0;
+	}
+	if ((con->minus == 1 || con->minus_pre == 1))
+		*ret = s_prin(str, con, len, ret);
+	*ret = print_morz(con, len, ret);
 	if (con->minus == 0 && con->minus_pre == 0)
-		*ret = n_prin(str, con, len, ret);
+		*ret = s_prin(str, con, len, ret);
 	return (*ret);
 }
