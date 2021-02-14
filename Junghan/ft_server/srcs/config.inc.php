984e4123d7341acf2c99d3707515d22bca1db770
james <gkswndaud@gmail.com>
2021-02-14 18:08:24 +0900

    finished server

---
+++ Junghan/ft_server/srcs/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+
+$cfg['blowfish_secret'] = 'KRZzjzjwaQGiF44THfBP1Yy,4LvJz3;t';
+
+$i = 0;
+
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+
+
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
