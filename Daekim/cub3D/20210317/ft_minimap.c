7ec90d2edd548db57bab885151d6fd6b46fc2816
Daehyun Kim <daekim@c6r7s7.42seoul.kr>
2021-03-18 07:48:25 +0900

    minimap norm

--- Daekim/cub3D/20210317/ft_minimap.c
+++ Daekim/cub3D/20210317/ft_minimap.c
@@ -12,19 +12,44 @@
 
 #include "cub.h"
 
-void	get_pos(t_info *p, int j, int i)
+void		get_pos(t_info *p, int j, int i)
 {
 	if (j >= 2 * ((p->data.win_h / 6) / 5)
 			&& j <= 3 * ((p->data.win_h / 6) / 5))
 		if (i >= 3 * ((p->data.win_w / 3) / 7)
 				&& i <= 4 * ((p->data.win_w / 3) / 7))
-			p->minimap[j][i] =  0xFD93F9;
+			p->minimap[j][i] = 0xFD93F9;
+}
+
+void		color_mini(t_info *p, int i, int j)
+{
+	if ((j / ((p->data.win_h / 6) / 5) + (int)p->pos_x >= 2) &&
+		(j / ((p->data.win_h / 6) / 5) + (int)p->pos_x <= p->data.m_num + 1))
+	{
+		if (i / ((p->data.win_w / 3) / 7) + (int)p->pos_y >= 3 &&
+			i <= (p->data.win_w - ((p->data.win_w / 3) / 7) * 2))
+		{
+			if (p->data.map[j / ((p->data.win_h / 6) / 5) + (int)p->pos_x - 2]
+				[i / ((p->data.win_w / 3) / 7) + (int)p->pos_y - 3] == '1')
+				p->minimap[j][i] = 0xFF0000;
+			else if (p->data.map[(j / ((p->data.win_h / 6) / 5) +
+					(int)p->pos_x) - 2][i / ((p->data.win_w / 3) / 7) +
+					(int)p->pos_y - 3] == '2')
+				p->minimap[j][i] = 0x00FF00;
+			else if (p->data.map[j / ((p->data.win_h / 6) / 5) +
+					(int)p->pos_x - 2][i / ((p->data.win_w / 3) / 7) +
+					(int)p->pos_y - 3] == '0')
+				p->minimap[j][i] = 0x0000FF;
+		}
+	}
+	else
+		p->minimap[j][i] = 0x0000FF;
 }
 
 void		show_mini(t_info *p)
 {
-	int	i;
-	int j;
+	int		i;
+	int		j;
 
 	j = -1;
 	while (++j < p->data.win_h / 6)
@@ -32,34 +57,17 @@ void		show_mini(t_info *p)
 		i = -1;
 		while (++i < p->data.win_w / 3)
 		{
-			if ((j / ((p->data.win_h / 6) / 5) + (int)p->pos_x >= 2) &&
-					(j / ((p->data.win_h / 6) / 5) + (int)p->pos_x <= p->data.m_num + 1))
-			{
-				if (i / ((p->data.win_w / 3) / 7) + (int)p->pos_y >= 3 &&
-					i <= (p->data.win_w - ((p->data.win_w / 3) / 7) * 2))
-				{
-					if (p->data.map[j / ((p->data.win_h / 6) / 5) + (int)p->pos_x - 2]
-							[i / ((p->data.win_w / 3) / 7) + (int)p->pos_y - 3] == '1')
-						p->minimap[j][i] = 0xFF0000;
-					else if (p->data.map[(j / ((p->data.win_h / 6) / 5) + (int)p->pos_x) - 2]
-							[i / ((p->data.win_w / 3) / 7) + (int)p->pos_y - 3] == '2')
-						p->minimap[j][i] = 0x00FF00;
-					else if (p->data.map[j / ((p->data.win_h / 6) / 5) + (int)p->pos_x - 2]
-							[i / ((p->data.win_w / 3) / 7) + (int)p->pos_y - 3] == '0')
-						p->minimap[j][i] = 0x0000FF;
-				}
-			}
-			else
-				p->minimap[j][i] = 0x0000FF;
+			color_mini(p, i, j);
 			get_pos(p, j, i);
 		}
 	}
+	printf("%d\n", p->data.m_num);
 }
 
-void	map_base(t_info *p)
+void		map_base(t_info *p)
 {
-	int	i;
-	int j;
+	int		i;
+	int		j;
 
 	j = 0;
 	while (j < p->data.win_h / 6)
